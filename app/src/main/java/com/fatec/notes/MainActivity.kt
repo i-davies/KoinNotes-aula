97476b211bd58a087941798c807fc39f4b25f016
Icaro Davies <idavies@outlook.com>
2026-05-07 19:17:41 -0300

    feat: Injeção de Dependências com Koin

--- app/src/main/java/com/fatec/notes/MainActivity.kt
+++ app/src/main/java/com/fatec/notes/MainActivity.kt
@@ -11,12 +11,13 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import com.fatec.notes.ui.screens.NoteScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContent { }
+        setContent { NoteScreen() }
     }
 }
 
---
+++ app/src/main/java/com/fatec/notes/ui/screens/NoteScreen.kt
@@ -0,0 +1,91 @@
+package com.fatec.notes.ui.screens
+
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.statusBarsPadding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.HorizontalDivider
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.fatec.notes.ui.components.NoteItem
+import com.fatec.notes.viewmodel.NoteViewModel
+import org.koin.compose.viewmodel.koinViewModel
+
+@Composable
+fun NoteScreen (viewModel: NoteViewModel = koinViewModel()) {
+    val state by viewModel.uiState.collectAsState()
+
+    MaterialTheme {
+        Surface (modifier = Modifier.fillMaxSize()) {
+            Column (
+                modifier = Modifier.fillMaxSize().statusBarsPadding().padding(16.dp),
+                verticalArrangement = Arrangement.spacedBy(12.dp)
+            ) {
+                Text("Minhas Notas", fontSize = 24.sp, fontWeight = FontWeight.Bold)
+                HorizontalDivider()
+                Text("${state.totalNotes} nota(s)", fontSize = 14.sp,
+                    color = MaterialTheme.colorScheme.onSurfaceVariant)
+
+                OutlinedTextField(
+                    value = state.inputTitle,
+                    onValueChange = {viewModel.onTitleChange(it)},
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = {Text("Título da nota...")},
+                    singleLine = true, shape = RoundedCornerShape(8.dp)
+                )
+                OutlinedTextField(
+                    value = state.inputContent,
+                    onValueChange = {viewModel.onContentChange(it)},
+                    modifier = Modifier.fillMaxWidth(),
+                    placeholder = {Text("Conteúdo (opcional)...")},
+                    minLines = 2, maxLines = 3, shape = RoundedCornerShape(8.dp)
+                )
+
+                Button(onClick = {viewModel.addNote()}, modifier = Modifier.fillMaxWidth()) {
+                    Text("Adicionar Nota")
+                }
+
+                if (state.isEmpty) {
+                    Box(Modifier.fillMaxWidth().weight(1f), contentAlignment = Alignment.Center) {
+                        Text("Nenhuma nota criada", color = MaterialTheme.colorScheme.onSurfaceVariant)
+                    }
+                }
+                else {
+                    LazyColumn(verticalArrangement = Arrangement.spacedBy(8.dp),
+                        modifier = Modifier.weight(1f)) {
+                        items(items = state.notes, key = { it.id }) { note ->
+                            NoteItem(
+                                note = note,
+                                onDelete = { viewModel.removeNote(note.id)},
+                                modifier = Modifier.animateItem(
+                                    fadeInSpec = tween(300),
+                                    fadeOutSpec = tween(300)
+                                )
+                            )
+
+                        }
+                    }
+
+                }
+            }
+        }
+    }
+}
